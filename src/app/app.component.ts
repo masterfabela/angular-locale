a87fddb6601a89841c165bbf659f9245535b5eb6
masterfabela <fabelaretorcida@gmail.com>
2019-11-15 12:19:07 +0100

    Configuracion de exclision de linter de typescript

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,13 @@
 import { Component } from '@angular/core';
+import {TranslateService} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
-})
+  templateUrl: './app.component.html'})
 export class AppComponent {
-  minutes = 0;
-  gender = 'female';
-  fly = true;
-  logo = 'https://angular.io/assets/images/logos/angular/angular.png';
-  inc(i: number) {
-    this.minutes = Math.min(5, Math.max(0, this.minutes + i));
+  constructor(public translate: TranslateService) {
+    translate.addLangs(['es', 'en']);
+    translate.setDefaultLang('es');
   }
-  male() { this.gender = 'male'; }
-  female() { this.gender = 'female'; }
-  other() { this.gender = 'other'; }
-}
 
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { TranslateModule } from '@ngx-translate/core';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  imports: [ BrowserModule ],
+  imports: [
+    BrowserModule,
+    TranslateModule,
+  ],
+  bootstrap: [ AppComponent ],
   declarations: [ AppComponent ],
-  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
