f578500be150300609f7dcc3eebd382ba36a6354
masterfabela <fabelaretorcida@gmail.com>
2019-11-15 09:08:40 +0100

    Configuracion añadida en ingles y frances

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { registerLocaleData } from '@angular/common';
-import localeFr from '@angular/common/locales/fr';
 
 @NgModule({
   imports: [ BrowserModule ],
